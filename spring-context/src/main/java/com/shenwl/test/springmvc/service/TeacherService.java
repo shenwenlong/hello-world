fc7c351a4714c015f04af4abffdb3aa4dfe1b3a8
shenwl <smells2@sina.com>
2018-11-01 17:04:27 +0800

    no modified

--- spring-context/src/main/java/com/shenwl/test/springmvc/service/TeacherService.java
+++ spring-context/src/main/java/com/shenwl/test/springmvc/service/TeacherService.java
@@ -1,34 +1,44 @@
 package com.shenwl.test.springmvc.service;
 
+import java.io.UnsupportedEncodingException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.shenwl.test.springmvc.domain.TeacherDomain;
 import com.shenwl.test.springmvc.entity.TeacherEntity;
 
-@Controller
+@RestController
 public class TeacherService {
 
 	Log log = LogFactory.getLog(TeacherService.class);
 	@Autowired
 	TeacherDomain teacherDomain;
 	
-	@RequestMapping(value="/teacher/getTeahcer", method=RequestMethod.GET)
-	public void getTeacherService(@RequestParam(value="id") String id) {
+	@RequestMapping(value="rest/teacher/getTeahcer", method=RequestMethod.POST)
+	@ResponseBody
+	public TeacherEntity getTeacherService(@RequestBody TeacherEntity request) throws Exception {
+		TeacherEntity te = null;
+		log.debug("param["+request.getId()+"]");
 		try {
 			log.debug(this.getClass().getSimpleName()
 					+"[getTeacherService()]");
-			TeacherEntity te = teacherDomain.getTeacher(id);
+			String id = request.getId().toString();
+			te = teacherDomain.getTeacher(id);
 			log.debug("teacher name:["+te.getName()+"]");
 			
 		} catch (SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return te;
 	}
 }
